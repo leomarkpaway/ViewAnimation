7e05e1311400a1d65db7ef012bbad2dcaf72d1c8
leomarkpaway <leomarkpaway@gmail.com>
2024-07-30 14:42:31 +0800

    :sparkles: Create upward fade animation

--- app/src/main/java/com/leomarkpaway/viewanimation/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/viewanimation/MainActivity.kt
@@ -2,10 +2,17 @@ package com.leomarkpaway.viewanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val upwardFadeButton = findViewById<Button>(R.id.btn_upward)
+        val helloWord = findViewById<TextView>(R.id.tv_hello_world)
+
+        upwardFadeButton.setOnClickListener { startAnimation(helloWord, R.anim.upward_fade) }
     }
 }
